4329ab2588a5b0e0f01ed58faf11ece1f116f840
prometheus <2022296soeung@aupp.edu.kh>
2023-11-19 01:28:46 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from "./components/Navbar";
+import TableExp from "./components/Table";
+import AddExp from './functions/AddExp';
+import AddInc from './functions/AddInc';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let App = () => {
+    return (
+        <>
+        <div className='items-center'>
+        <Router>
+            <Navbar/>
+            <Routes>
+                <Route exact path="/" element={<TableExp/>}/>
+                <Route exact path="/exp" element={<AddExp/>}/>
+                <Route exact path="/inc" element={<AddInc/>}/>
+
+            </Routes>
+        </Router>
+        </div>
+        </>
+    );
 }
 
 export default App;
